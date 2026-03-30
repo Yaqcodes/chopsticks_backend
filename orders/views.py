fd42362d248972099e81cf40aae880b8c7ee6f85
Yaqcodes <shuaibkhaleefa@gmail.com>
2026-03-30 22:47:52 +0100

    bug fix

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 import logging
 from rest_framework import generics, status, filters
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdmin, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.db import transaction
